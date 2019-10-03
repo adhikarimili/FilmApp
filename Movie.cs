5ef164d64e8302c85001d7d7ec422736e2993867
ptamang <adhikarimili2019@gmail.com>
2019-10-03 11:18:40 -0700

    Initial commit

---
+++ Movie.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmApp
+{/// <summary>
+/// This is a Film lebrary where user can add movies and give rating.
+/// </summary>
+
+    class Movie
+    {
+        #region Variable
+
+        /// <summary>
+        /// Autogenrate a Movie numbber
+        /// </summary>
+
+        public static int lastMovieNumber = 0;       
+
+
+        #endregion
+
+        #region Properties
+            
+        public string  MovieNumber { get; set; }
+        /// <summary>
+        /// Name of the movie
+        /// </summary>
+        public string MovieName { get; set; }
+
+        /// <summary>
+        /// lenght of movie
+        /// </summary>
+        public int MovieLenght { get; set; }
+
+        /// <summary>
+        /// Rating of the Movie
+        /// </summary>
+        public Decimal MovieRating { get; set; }
+
+        /// <summary>
+        /// Relase Date of the Movie
+        /// </summary>
+        public DateTime MovieRelaseDate { get; set; }
+
+        #endregion
+
+        #region Constructor
+        public Movie()
+        {
+            MovieNumber = lastMovieNumber++;
+            MovieRelaseDate = DateTime.Now;
+        }
+        #endregion
+
+        #region Method
+        ///<summary>
+        /// new Movie added in the movie library
+        ///</summary>
+        ///<param name = "movie is added"></param>
+        
+        public void LastMovieNumber(int movie)
+        {
+            MovieNumber += movie;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FilmApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instance of an Movie == Object
+            var movie = new Movie
+            {
+                MovieNumber = "1234",
+                MovieName = "abcdef"
+            };
+            movie.LastMovieNumber(1);
+
+            Console.WriteLine($"MovieNumber: {movie.MovieNumber}," +
+                $" MovieName : {movie.MovieName}," +
+                $" MovieRating: {movie.MovieRating}, " +
+                $"MovieRaleaseDate: {movie.MovieRelaseDate}, " +
+                $"MovieLenght: {movie.MovieLenght}");
+        }
+    }
+}
+// account.Deposit(2003.234m)
